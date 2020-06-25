346064868e9f4d322a6df960c5009fd331ab6ddf
jplck <pollack.jan@gmail.com>
2020-06-25 13:08:45 +0200

    rebuild folder strcuture

---
+++ csharp/README.md
@@ -0,0 +1,1 @@
+# devops-ticket-translation-function
---
+++ csharp/WebHook.cs
@@ -0,0 +1,115 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System.Net.Http;
+using System.Net;
+
+namespace tickettranslator
+{
+    public static class WebHook
+    {
+        public class DevOpsPayload
+        {
+
+            [JsonProperty("message")]
+            public DevOpsMessagePayload Message { get; set; }
+
+        }
+
+        public class DevOpsMessagePayload
+        {
+
+            [JsonProperty("text")]
+            public string Text { get; set; }
+
+            [JsonProperty("html")]
+            public string HTML { get; set; }
+
+            [JsonProperty("markdown")]
+            public string Markdown { get; set; }
+
+        }
+
+        [FunctionName("webhook")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "webhook")] HttpRequest req,
+            [DurableClient] IDurableOrchestrationClient client,
+            ILogger log)
+        {
+            log.LogInformation("Hook has been called.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<DevOpsPayload>(requestBody);
+
+            await client.StartNewAsync("ModelOrchestrator", data);
+
+            return new OkObjectResult(requestBody);
+        }
+
+        [FunctionName("ModelOrchestrator")]
+        public static async Task ModelOrchestratorAsync(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            var input = context.GetInput<DevOpsPayload>();
+
+            var validationResult = await context.CallActivityAsync<string>("CallTranslationServiceFunction", input);
+            await context.CallActivityAsync<bool>("UpdateDevOpsTicketFunction", validationResult);
+        }
+
+        [FunctionName("CallTranslationServiceFunction")]
+        public static Task<string> CallTranslationServiceFunction([ActivityTrigger] DevOpsPayload payload,
+            ILogger log)
+        {
+            var apimKey = Environment.GetEnvironmentVariable("ENDPOINT_SECRET");
+            var region = Environment.GetEnvironmentVariable("ENDPOINT_REGION");
+            var endpoint = Environment.GetEnvironmentVariable("TRANSLATION_ENDPOINT");
+            var apiVersion = "3.0";
+            var targetLang = "de";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{endpoint}/translate?api-version={apiVersion}&to={targetLang}");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Headers.Add($"Ocp-Apim-Subscription-Key:{apimKey}");
+            request.Headers.Add($"Ocp-Apim-Subscription-Region:{region}");
+            request.Headers.Add("charset: UTF-8");
+
+            using (Stream webStream = request.GetRequestStream())
+            using (StreamWriter requestWriter = new StreamWriter(webStream, System.Text.Encoding.ASCII))
+            {
+                requestWriter.Write(payload.Message.Text);
+            }
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
+                using (StreamReader responseReader = new StreamReader(webStream))
+                {
+                    string response = responseReader.ReadToEnd();
+                    return Task.FromResult(response);
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.Message);
+            }
+
+            return Task.FromResult(String.Empty);
+        }
+
+        [FunctionName("UpdateDevOpsTicketFunction")]
+        public static Task<bool> UpdateDevOpsTicketFunction([ActivityTrigger] string translatedContent,
+            ILogger log)
+        {
+            return Task.FromResult(true);
+        }
+
+    }
+}
